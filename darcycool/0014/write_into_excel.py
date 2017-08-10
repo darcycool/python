f5e5f0256259d916751ee2db6997720d80826109
darcy <answer134@126.com>
2017-08-10 15:06:45 +0800

    commit 0014

---
+++ darcycool/0014/write_into_excel.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 第 0014 题： 纯文本文件 student.txt为学生信息,请将上述内容写到 student.xls 文件中
+
+import json
+from collections import OrderedDict
+
+import xlwt
+
+file = open('student.txt', 'r')
+
+students = json.loads(file.read())
+
+wb = xlwt.Workbook()
+ws = wb.add_sheet('学生信息')
+
+json.dumps(OrderedDict(students))
+
+for index, items in enumerate(students.items()):
+    ws.write(index, 0, items[0])
+    for j, v in enumerate(items[1]):
+        ws.write(index, j + 1, v)
+
+wb.save('学生信息.xls')
